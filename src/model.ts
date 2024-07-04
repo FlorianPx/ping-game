9b324ccb59a20b61b258da1fd83f1c801c21b30a
Florian <68642839+FlorianPx@users.noreply.github.com>
2024-07-04 16:06:46 +0200

    feat: add prettier and postcss config

--- src/model.ts
+++ src/model.ts
@@ -1,15 +1,15 @@
-type X = number;
-type Y = number;
-type RadiusType = number;
-type ColorType = string;
+type X = number
+type Y = number
+type RadiusType = number
+type ColorType = string
 
 type VelocityType = {
-  x: X;
-  y: Y;
-};
+  x: X
+  y: Y
+}
 
 type PlayerDataType = {
-  score: number;
-  bestScore: number;
-  level: number;
-};
+  score: number
+  bestScore: number
+  level: number
+}
